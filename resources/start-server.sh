b94b2389e307c2a0455870a225f249b97f1ff4ae
Luiz Renato <luizrenato2005@gmail.com>
2018-10-22 13:20:47 -0300

    ajustes json-server

--- resources/start-server.sh
+++ resources/start-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
- /opt/node-8.11.3/lib/node_modules/json-server/bin/index.js banco.json --port 1234
+echo 'Iniciando json-server em http://localhost:1234'
+
+ /opt/node-8.11.3/lib/node_modules/json-server/bin/index.js banco.json --port 1234 --routes routes.json
 
